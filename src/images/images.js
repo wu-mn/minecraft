1be0798e2a74037ddea148f42d871e5ea6320ba6
wu-mn <wu_manning@retail-ai.jp>
2023-01-12 09:12:46 +0900

    ground and sky

---
+++ src/images/images.js
@@ -0,0 +1,9 @@
+import dirtImg from "./dirt.jpg";
+import glassImg from "./glass.png";
+import grassImg from "./grass.jpg";
+import logImg from "./log.jpg";
+import woodImg from "./wood.png";
+
+export { dirtImg, glassImg, grassImg, logImg, woodImg };
+
+// export default images;
---
+++ src/images/textures.js
@@ -0,0 +1,18 @@
+import { TextureLoader } from "three";
+import { dirtImg, glassImg, grassImg, logImg, woodImg } from "./images";
+
+const dirtTexture = new TextureLoader().load(dirtImg);
+const logTexture = new TextureLoader().load(logImg);
+const grassTexture = new TextureLoader().load(grassImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const woodTexture = new TextureLoader().load(woodImg);
+const groundTexture = new TextureLoader().load(grassImg);
+
+export {
+  dirtTexture,
+  logTexture,
+  grassTexture,
+  glassTexture,
+  woodTexture,
+  groundTexture,
+};
